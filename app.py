41516364c1c4a90881d1082d708e536162a74820
Fabio Camara <fabio.camara@outlook.com>
2023-09-21 20:27:35 -0300

    updates

---
+++ app.py
@@ -0,0 +1,42 @@
+#Autor: Fabio Camara
+
+#pip install flask
+from flask import Flask, jsonify
+
+#pip install python-dotenv
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+#pip install flask_mysqldb
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.environ.get("HOST")
+app.config['MYSQL_USER'] = os.environ.get("USER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("PASSWORD")
+app.config['MYSQL_DB'] = os.environ.get("DATABASE")
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM contatos")
+    result = cursor.fetchall()
+    cursor.close()
+    for x in result:
+        print(x)
+
+    #return jsonify(result)
+    return jsonify(user="lala")
+
+@app.route('/contato/<id>')
+def contato(id):
+    cursor = mysql.connection.cursor()
+    sql = "SELECT * FROM contatos WHERE idcontato = %s"
+    cursor.execute(sql, id)
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify(result)
